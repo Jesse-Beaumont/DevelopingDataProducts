af3ebc9037d70f6294686aa411567b89653cf2b7
Jesse Beaumont <jbeaumont@d-wise.com>
2017-04-02 17:35:04 -0400

    minor changes

--- global.R
+++ global.R
@@ -1,5 +1,8 @@
-
-
+#------------------------------------------------
+#
+#  Global variables for Shiny Application
+#    - source CSV dataset
+#
+#------------------------------------------------
 df_states <- read.csv(file="./State_Under_65_Lite.csv", head=TRUE, sep=",")
 colnames(df_states) <- c("STATE","AMB_USERS","AMB_RATE","AMB_EVENTS_PER_K","READMITS","READMIT_RATE","ER_VISITS","ER_VISITS_PER_K")
-
--- ui.R
+++ ui.R
@@ -1,43 +1,50 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
+#  Shiny application renders a choropleth of US States
+#  allowing comparison of ER visits, Readmits, and Ambulance Use.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(plotly)
-library(shinythemes)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(
+   fluidPage(
 
   # Application title
-  titlePanel("US Utilization Rate ER, Readmits, and Ambulance"),
+  titlePanel("2014 US Utilization of Emergency Rooms, Readmits, and Ambulances"),
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("er_visits", "Emergency Room Visits (per 1000):", min = min(df_states$ER_VISITS_PER_K),
-                                                                     max = max(df_states$ER_VISITS_PER_K),
-                                                                   value = min(df_states$ER_VISITS_PER_K), step = 100),
+  fluidRow(
 
-       sliderInput("readmit_rate", "Readmittance Rate:", min = min(df_states$READMIT_RATE),
-                                                         max = max(df_states$READMIT_RATE),
-                                                       value = min(df_states$READMIT_RATE), step = 0.01),
+     column(width=4,
+         h4("Emergency department visits (per 1000 beneficiaries)"),
+         plotlyOutput("erPlot")
+     ),
+     column(width=4,
+         h4("% inpatient readmissions within 30 days of an acute hospital stay"),
+         plotlyOutput("readmitPlot")
+     ),
+     column(width=4,
+        h4("% beneficiaries using Ambulance services"),
+        plotlyOutput("ambPlot")
+     )
 
-       sliderInput("amb_util_rate", "Ambulance Utilizaiton Rate:", min = min(df_states$AMB_RATE),
-                                                                   max = max(df_states$AMB_RATE),
-                                                                 value = min(df_states$AMB_RATE), step = 0.01)
-    , width = 4),
+  ),
 
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotlyOutput("erPlot"),
-       plotlyOutput("readmitPlot"),
-       plotlyOutput("ambPlot")
-    )
+  fluidRow(
+     column(width=4,
+            sliderInput("er_visits", "Emergency Room Visits (per 1000):", min = min(df_states$ER_VISITS_PER_K),
+                        max = max(df_states$ER_VISITS_PER_K),
+                        value = min(df_states$ER_VISITS_PER_K), step = 100)
+     ),
+     column(width=4,
+            sliderInput("readmit_rate", "Readmittance Rate:", min = min(df_states$READMIT_RATE),
+                        max = max(df_states$READMIT_RATE),
+                        value = min(df_states$READMIT_RATE), step = 0.01)
+     ),
+     column(width=4,
+            sliderInput("amb_util_rate", "Ambulance Utilizaiton Rate:", min = min(df_states$AMB_RATE),
+                        max = max(df_states$AMB_RATE),
+                        value = min(df_states$AMB_RATE), step = 0.01)
+     )
   )
 ))
+
